7b105907018f8d99f446c00e8be928d7cc1431c9
oliveiraFab16 <fabriciocarvalho932@gmail.com>
2020-10-09 18:11:19 -0300

    HoraSistema

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Figgle;
+using Humanizer;
+
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Figgle: HoraSistema Com Pacotes!");
+            Console.WriteLine(FiggleFonts.Standard.Render("Bom Dia"));
+            Console.ReadKey();
+            
+            
+            Console.WriteLine("--- Pelo Horario De Brasília Agora São:");
+            DateTime horaSistema = DateTime.Now;
+            Console.WriteLine($"Exatamente: {horaSistema}");
+
+
+
+        }
+    }
+}
